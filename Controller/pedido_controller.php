212c8025316b95ea9da412fa45a169e821c9b64f
daniellimberger <daniellimberger@hotmail.com>
2017-09-25 02:36:47 +0200

    CREATE, RETRIEVE e DELETE para a tela de Pedidos

---
+++ Controller/pedido_controller.php
@@ -0,0 +1,193 @@
+<?php  header('Access-Control-Allow-Origin: *');  
+
+require_once '../Model/database.php';
+require_once '../Model/pedido.php';
+
+$pedidoController = new PedidoController();
+
+$function = $_GET['function'];
+
+switch ( $function ){
+  case 'listar_todos':
+    $retorno = $pedidoController->listar_todos();
+    break;
+
+  case 'listar_todos_pedidoItem':
+    $retorno = $pedidoController->listar_todos_pedidoItem();
+    break;
+
+  case 'deletar_pedido_item':
+    $retorno = $pedidoController->deletar_pedido_item();
+    break;
+
+
+
+  case 'cadastrar':
+    $retorno = $pedidoController->cadastrar();
+    echo $retorno;
+    break;
+
+  case 'cadastrar_item':
+    $retorno = $pedidoController->cadastrar_item();
+    echo $retorno;
+    break;
+
+
+  case 'deletar':
+    $retorno = $pedidoController->deletar();
+    echo $retorno;  
+    break;
+  case 'listar_um':
+    $id_listar = $_GET['id_listar'];
+    $retorno = $pedidoController->listar_um($id_listar);
+   //echo $retorno;  
+   // echo $id_listar;
+    break;
+}
+
+echo $retorno;
+
+class PedidoController{
+
+    private $model;
+
+    public function __CONSTRUCT(){
+        $this->model = new pedido();
+    }
+
+
+    public function cadastrar(){
+
+        $obj = json_decode(file_get_contents('php://input'));
+
+        $idCliente  = $obj->idCliente;
+        $idVendedor = $obj->idVendedor;
+        $dataPedido = $obj->dataPedido;
+        $dataPrevisaoEntrega  = $obj->dataPrevisaoEntrega;
+        $valorTotal = $obj->valorTotal;
+  
+
+        $pedido = new pedido();
+        // instanciando classe da model
+
+        // inserindo dados no atributo da classe da model $prod->empresa
+        $pedido->idCliente = $idCliente;
+        $pedido->idVendedor = $idVendedor;
+        $pedido->dataPedido = $dataPedido;
+        $pedido->dataPrevisaoEntrega = $dataPrevisaoEntrega;
+        $pedido->valorTotal = $valorTotal;
+
+        $return = $pedido->Cadastrar();
+
+        //return $return;
+        echo $return;
+
+    }
+    
+
+
+
+    public function cadastrar_item(){
+
+        $obj = json_decode(file_get_contents('php://input'));
+
+        $nro_pedido     = $obj->nro_pedido;
+        $idProduto      = $obj->idProduto;
+        $qtdItem        = $obj->qtdItem;
+        $valorUnit      = $obj->valorUnit;
+        $valorUnitTotal = $obj->valorUnitTotal;
+  
+
+        $pedido = new pedido();
+        // instanciando classe da model
+
+        // inserindo dados no atributo da classe da model $prod->empresa
+        $pedido->item_nro_pedido = $nro_pedido;
+        $pedido->item_idProduto = $idProduto;
+        $pedido->item_qtdItem = $qtdItem;
+        $pedido->item_valorUnit = $valorUnit;
+        $pedido->item_valorUnitTotal = $valorUnitTotal;
+
+        $return = $pedido->CadastrarItem();
+
+        //return $return;
+        echo $return;
+
+    }    
+
+    public function deletar_pedido_item(){
+
+        $obj = json_decode(file_get_contents('php://input'));
+
+         $id_deletar          = $obj->id_deletar;      
+
+       $dados = $this->model->DeletarPedidoItem($id_deletar);
+
+       return  json_encode($id_deletar);
+    }   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    public function listar_todos(){
+
+       $dados = $this->model->ListarTodos();
+
+       return  json_encode($dados);
+    }    
+
+
+    public function listar_todos_pedidoItem(){
+
+       $dados = $this->model->ListarTodos_PedidoItem();
+
+       return  json_encode($dados);
+    }       
+
+
+
+
+
+
+
+
+
+
+
+
+    public function deletar(){
+
+        $obj = json_decode(file_get_contents('php://input'));
+
+         $id_deletar          = $obj->id_deletar;      
+
+       $dados = $this->model->Deletar($id_deletar);
+
+       return  json_encode($dados);
+    }
+
+    public function listar_um($id_listar){
+
+
+       $dados = $this->model->ListarUm($id_listar);
+
+       return  json_encode($dados);
+
+    }                
+
+}
---
+++ Model/pedido.php
@@ -0,0 +1,157 @@
+<?php
+class pedido
+{
+	private $pdo;
+
+	public $idCliente;
+	public $idVendedor;
+	public $dataPedido;
+	public $dataPrevisaoEntrega;
+	public $valorTotal;
+
+    public $item_nro_pedido;
+	public $item_idProduto;
+    public $item_qtdItem;
+    public $item_valorUnit;
+    public $item_valorUnitTotal;
+
+
+	public function __CONSTRUCT()
+	{
+		try
+		{	// do arquivo model/database.php
+			// este arquivo está incluso no controller
+			$this->pdo = Database::Conectar();
+		}
+		catch(Exception $e)
+		{
+			die($e->getMessage());
+		}
+	}
+
+
+
+
+	public function Cadastrar()	
+	{
+		try
+		{
+		$sql = "INSERT INTO pedido (fk_cliente, fk_vendedor, data_pedido, data_previsao_entrega, valor_total)
+		        VALUES (?, ?, ?, ?, ?)";
+
+		$this->pdo->prepare($sql)
+		     ->execute(
+				array(
+                    $this->idCliente,
+					$this->idVendedor,
+                    $this->dataPedido,
+                    $this->dataPrevisaoEntrega,                    
+                    $this->valorTotal
+                )
+			);
+		
+			$last_id = $this->pdo->lastInsertId();
+			//return $last_id;
+			echo $last_id;
+
+
+		} catch (Exception $e)		
+		{
+			die($e->getMessage());
+		}
+	}
+
+
+	public function CadastrarItem()	
+	{
+		try
+		{
+		$sql = "INSERT INTO pedido_item (id_pedido, id_produto, quantidade, valor_unitario, valor_unitario_total)
+		        VALUES (?, ?, ?, ?, ?)";
+
+		$this->pdo->prepare($sql)
+		     ->execute(
+				array(
+                    $this->item_nro_pedido,
+					$this->item_idProduto,
+                    $this->item_qtdItem,
+                    $this->item_valorUnit,                    
+                    $this->item_valorUnitTotal
+                )
+			);
+
+	
+			$last_id = $this->pdo->lastInsertId();
+			//return $last_id;
+			echo $last_id;
+
+
+		} catch (Exception $e)		
+		{
+			die($e->getMessage());
+		}
+	}
+
+
+
+
+
+	public function ListarTodos_PedidoItem()
+	{
+		try
+		{
+			$result = array();
+
+			$stm = $this->pdo->prepare("SELECT pi.id as pi_id,
+										p.nome as item_produto, 
+										pi.quantidade as item_quantidade, 
+										pi.valor_unitario as item_valor_unitario, 
+										pi.valor_unitario_total as item_valor_unitario_total 
+										FROM pedido_item pi
+										INNER JOIN produto p ON pi.id_produto = p.id
+										WHERE pi.id_pedido = '38'
+
+
+				");
+			$stm->execute();
+
+			return $stm->fetchAll(PDO::FETCH_OBJ);
+		}
+		catch(Exception $e)
+		{
+			die($e->getMessage());
+		}
+	}
+
+	public function DeletarPedidoItem($id_deletar)
+	{
+		try
+		{
+			$stm = $this->pdo
+			            ->prepare("DELETE FROM pedido_item WHERE id = ?");
+
+			$stm->execute(array($id_deletar));
+		} catch (Exception $e)
+		{
+			die($e->getMessage());
+		}
+	}		
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}	
