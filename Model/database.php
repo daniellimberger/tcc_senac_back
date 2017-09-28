47937d83a45861e44826ac332f10ca2c3ef1d161
ldaazzi@gmail.com <daniellimberger@hotmail.com>
2017-09-28 21:44:51 +0200

    ajuste na  conexao com o banco de dados

--- Model/database.php
+++ Model/database.php
@@ -4,7 +4,7 @@ class Database
 
     public static function Conectar()
     {
-        $pdo = new PDO('mysql:host=mysql.ligiano.info;dbname=ligiano05;charset=utf8', 'ligiano05', '301020ab');
+        $pdo = new PDO('mysql:host=mysql02-farm70.uni5.net;dbname=tccdaniel;charset=utf8', 'tccdaniel', '301020ab');
         //Filtrando possiveis erros de conexao
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
